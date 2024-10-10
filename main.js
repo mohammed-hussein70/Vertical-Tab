1dcf534447bc5d30acbe9a18ed63d76ecebbde13
mohammed <mohammedhussein004@gmail.com>
2024-10-10 17:16:49 +0300

    Add files

---
+++ main.js
@@ -0,0 +1,11 @@
+let buttons = document.querySelectorAll("button");
+let text = document.querySelectorAll(".tab-text");
+
+buttons.forEach((items, index) => {
+  items.addEventListener("click", function () {
+    text.forEach((item) => {
+      item.classList.add("hidden");
+    });
+    text[index].classList.remove("hidden");
+  });
+});
